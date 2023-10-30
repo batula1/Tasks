1072d555725afc6104bbe560e8768b1ebc892b36
Albert Batula <batula227@gmail.com>
2023-10-30 09:08:39 +0300

    Реализовал подключение к БД

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,48 @@
 package jm.task.core.jdbc.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private Util() {
+
+    }
+
+
+    private static Properties getProperties() {
+        Properties properties = new Properties();
+        try (InputStream in = Util.class.getClassLoader().getResourceAsStream("application.properties")) {
+            properties.load(in);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+
+    public static void getConnection() {
+        Properties prop = getProperties();
+        try (Connection conn = DriverManager.getConnection(prop.getProperty("db.url"),
+                prop.getProperty("db.username"),
+                prop.getProperty("db.password"))){
+            if(conn != null){
+                System.out.println("connection");
+            }else {
+                System.out.println("connection failed ");
+            }
+
+        } catch (SQLException exp){
+            System.err.format("SQL State: %s\n%s", exp.getSQLState(), exp.getMessage());
+        } catch (Exception exp){
+            exp.printStackTrace();
+        }
+    }
 }
+
+
+
