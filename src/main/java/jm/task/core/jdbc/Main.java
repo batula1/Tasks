22760e76ad603fbf00541b4042dd95c28b9356e1
Albert Batula <batula227@gmail.com>
2023-11-08 18:48:17 +0300

    done

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,11 +2,11 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
 
 public class Main {
-        private static final UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
+           UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Jensen","Ackles", (byte) 45);
         userService.saveUser("Misha","Collins", (byte) 49);
@@ -15,7 +15,6 @@ public class Main {
         userService.getAllUsers();
         userService.cleanUsersTable();
         userService.dropUsersTable();
-
 
         }
 }
